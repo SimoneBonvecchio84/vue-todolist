3287590242a0c7389306bd3b76c4b92280ae9743
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-04-20 16:32:49 +0200

    Feat Milestone1 and Milestone 2

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const { createApp } = Vue;
+const app = createApp({
+    data(){
+        return{
+            newTodo: {
+                text: "",
+                done: false
+            },
+          
+            todoList: [
+                
+                {
+                    text: "Recruiting blog post",
+                    done: true
+                },
+                {
+                    text: "Mobile app luaunch",
+                    done: true
+                },
+                {
+                    text: "Interview John H.",
+                    done: false
+                },
+                {
+                    text: "Summit update to mobile storefronts",
+                    done: true
+                }
+            ],
+        };
+    },
+    methods: {
+        // funzione che assegna la classe dinamica done al click
+        // o la toglie applicando la logica toogle
+        doneItems: function(i) {
+            console.log("aaaa")
+            // se l elemento corrente todoList(oggetto) ha la classe
+            // strettamente uguale a true allora applica false
+            // così facendo il il cerchio da verse passa a rosso
+            // e viceversa applicando anche la barra al text
+            // altrimenti rimani true
+             if(this.todoList[i].done === true){
+                
+                 this.todoList[i].done = false;
+            
+             } else {
+                 this.todoList[i].done = true;
+             }
+        },
+        // Ora implemento la funzione per aggiungere un nuovo object
+        // alla lista nel dom
+        // test con log se funziona il bottone
+        addItems: function () {
+
+        }
+    },
+}).mount("#app");
